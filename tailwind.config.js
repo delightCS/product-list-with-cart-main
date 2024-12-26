a04ba6d94a94cd495cf5f5c11df532a3324f3c3a
Chinenye <badnenye@gmail.com>
2024-12-26 15:24:13 +0100

    Resposive for small phones

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["index.html", "index.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          red: "hsl(14, 86%, 42%)",
+          green: "hsl(159, 69%, 38%)",
+        },
+        secondary: {
+          50: "hsl(20, 50%, 98%)",
+          100: "hsl(13, 31%, 94%)",
+          300: "hsl(14, 25%, 72%)",
+          400: "hsl(7, 20%, 60%)",
+          500: "hsl(12, 20%, 44%)",
+          900: "hsl(14, 65%, 9%)",
+        },
+      },
+      fontFamily: {
+        body: "Red Hat Text",
+      },
+    },
   },
   plugins: [],
 };
